18912cff19c903e015b517b6590bfb620a926148
karmasakshi <karmasakshi@outlook.com>
2015-04-29 17:07:21 +0530

    Added home page.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,23 +8,26 @@
  *
  * Main module of the application.
  */
+
 angular
-  .module('edApp', [
-    'ngAnimate',
-    'ngRoute',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .module('edApp', [
+            'ngAnimate',
+            'ngRoute',
+            'ngTouch'
+        ])
+        .config(function ($routeProvider) {
+            $routeProvider
+                    .when('/', {
+                        templateUrl: 'views/main.html',
+                        controller: 'MainCtrl'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+        }).run(function ($rootScope) {
+    // After changing route
+    $rootScope.$on('$routeChangeSuccess', function () {
+        // Initialize Foundation
+        $(document).foundation();
+    });
+});
